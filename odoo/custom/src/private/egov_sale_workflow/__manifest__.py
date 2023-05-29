e3131564313babfec960c6ca54563612d73dfaab
Pavarunc <Pavarunc@ecosoft.co.th>
2023-05-29 11:08:06 +0700

    [ADJ]move_analytic_account_id

---
+++ odoo/custom/src/private/egov_sale_workflow/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "EGOV - Sales ",
+    "version": "15.0.1.0.0",
+    "author": "Ecosoft",
+    "category": "EGOV",
+    "license": "AGPL-3",
+    "depends": ["sale"],
+    "data": [
+        "views/sale_order_view.xml",
+    ],
+    "installable": True,
+}
